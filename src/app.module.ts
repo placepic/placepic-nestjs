991c4e628b7a95ae2d148bed17fdf6282e31ecce
younghoon.choi <c_yh0327@naver.com>
2021-04-30 17:10:40 +0900

    [feat] users, cats 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { UsersModule } from './users/users.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, CatsModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
