c8b900c81ad444f8b25c81579ef70c2d760cdb2e
최영훈 <c_yh0327@naver.com>
2021-05-09 16:45:20 +0900

    logger 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,10 +6,15 @@ import { CatsController } from './cats/cats.controller';
 import { UsersModule } from './users/users.module';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [UsersModule, CatsModule, ConfigModule.forRoot()],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
