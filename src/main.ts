f6586b786ff71f59fe22f87a7aed508dabb05c51
최영훈 <c_yh0327@naver.com>
2021-05-09 15:05:02 +0900

    hot reloading

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
+  console.log('hello nest');
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
