86e83ecd523a49830e8ffa9ef675bb870e80337b
최영훈 <c_yh0327@naver.com>
2021-05-11 20:01:03 +0900

    [feat] DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,25 @@ import { UsersModule } from './users/users.module';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, CatsModule, ConfigModule.forRoot()], // 이 모듈에 필요한 공급자를 내보내는 가져온 모듈 목록
+  imports: [
+    UsersModule,
+    CatsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER_NAME,
+      password: process.env.DATABASE_PW,
+      database: 'placepic-typeORM',
+      charset: 'utf8mb4',
+      entities: [],
+      synchronize: true,
+    }),
+  ], // 이 모듈에 필요한 공급자를 내보내는 가져온 모듈 목록
   controllers: [AppController, CatsController], // 인스턴스화 되어야 하는 모듈에 정의된 컨트롤러 세트
   providers: [AppService, CatsService], // Nest 인젝터에 의해 인스턴스화되고 적어도 이 모듈에서 공유될 수 있는 공급자.
 })
