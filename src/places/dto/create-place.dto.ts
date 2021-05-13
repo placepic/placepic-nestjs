6bdf11a536a2f529c0cacb93b3aa43226c8a4991
younghoon.choi <c_yh0327@naver.com>
2021-05-13 18:26:16 +0900

    [refactor] update user DTO

--- src/places/dto/create-place.dto.ts
+++ src/places/dto/create-place.dto.ts
@@ -2,4 +2,12 @@ export class CreatePlaceDto {
   placeName: string;
   placeAddress: string;
   placeRoadAddress: string;
+  placeMapX: number;
+  placeMapY: number;
+  placeCreatedAt: number;
+  placeUpdatedAt: number;
+  userIdx: number;
+  placeReview: string;
+  categoryIdx: number;
+  groupIdx: number;
 }
