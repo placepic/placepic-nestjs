4b5d70ffc32db2200996f67ab8fdcfa1dbe29cd0
최영훈 <c_yh0327@naver.com>
2021-06-06 21:58:38 +0900

    [test] user findall

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,7 +3,8 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +12,15 @@ describe('UsersController', () => {
       providers: [UsersService],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe(' find All', () => {
+    it('should return an array of user', async () => {
+      const result = ['test'];
+      jest.spyOn(usersService, 'findAll').mockImplementation(() => result);
+      expect(usersController).toBeDefined();
+    });
   });
 });
