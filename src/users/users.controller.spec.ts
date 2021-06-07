c4951539fae460e1a532e9d69df1ddd8cdf69625
dudgns3tp <c_yh0327@naver.com>
2021-06-07 19:10:21 +0900

    [test] userservice beforeEach mockking

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
@@ -18,7 +19,28 @@ describe('UsersController', () => {
 
   describe('findAll', () => {
     it('should return an array of ussers', async () => {
-      const result = ['test'];
+      const result: Promise<User[]> = [
+        {
+          userIdx: 1,
+          email: '',
+          password: '',
+          salt: '',
+          userName: '',
+          userCreatedAt: 111,
+          phoneNumber: '',
+          certificationNumber: '',
+        },
+        {
+          userIdx: 2,
+          email: 'test@naver.com',
+          password: '1234',
+          salt: '1234',
+          userName: '최영훈',
+          userCreatedAt: 111,
+          phoneNumber: '01054099859',
+          certificationNumber: '1123',
+        },
+      ];
       jest.spyOn(usersService, 'findAll').mockImplementation(() => result);
       expect(await usersController.findAll()).toBe(result);
     });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+
+const mockUserRepository = () => ({
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  softDelete: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get<MockRepository<User>>(
+      getRepositoryToken(User),
+    );
   });
 
   it('should be defined', () => {
