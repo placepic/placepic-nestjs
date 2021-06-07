e47af21ce442d1aaae80009df40e2df0de6bdeb6
dudgns3tp <c_yh0327@naver.com>
2021-06-07 19:26:29 +0900

    [test] user Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,6 +3,8 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import * as faker from 'faker';
+import { create } from 'eslint/lib/rules/*';
 
 const mockUserRepository = () => ({
   save: jest.fn(),
@@ -34,7 +36,33 @@ describe('UsersService', () => {
     );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('User Service', () => {
+    describe('create()', () => {
+      const createArgs = {
+        userName: faker.lorem.sentence(),
+        email: faker.lorem.sentence(),
+        phoneNumber: faker.phone.phoneNumber(),
+        certificationNumber: '1234',
+        salt: 'hash',
+        password: faker.internet.password(),
+      };
+      it('should fail on exception', async () => {
+        usersRepository.save.mockRejectedValue('save error');
+        const result = await service.create(createArgs);
+        expect(result).toEqual('save error');
+      });
+      it('should create Users', async () => {
+        usersRepository.save.mockResolvedValue(createArgs);
+        const result = await service.create(createArgs);
+        expect(usersRepository.save).toHaveBeenCalledTimes(1); // save가 한번 불러졌니?
+        expect(usersRepository.save).toHaveBeenCalledWith(createArgs); // 매개변수가 createARgs가 같니?
+
+        expect(result).toEqual(createArgs);
+      });
+    });
+
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
   });
 });
